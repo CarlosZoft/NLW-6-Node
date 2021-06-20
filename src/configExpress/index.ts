45e93332d6d79718611e9c90d603e9c1fd3cac4c
Carlos <carlosoftwar@gmail.com>
2021-06-20 20:31:37 -0300

    chore: config express

---
+++ src/configExpress/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+export default () => {
+  const app = express();
+
+
+
+  return app;
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import express from './configExpress';
+const app = express();
+const port = 3030;
+
+app.listen(port, () => console.log("App running XD"));
