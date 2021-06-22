1ea0b502290adf2bb532927fe39c51def5c1dd9a
Carlos <carlosoftwar@gmail.com>
2021-06-22 01:12:59 -0300

    feat: add express-async-errors and endpoint users

--- src/configExpress/index.ts
+++ src/configExpress/index.ts
@@ -1,12 +1,17 @@
-import express from "express";
-import testRouter from '../routes';
-import "../database";
+import express, { NextFunction, Request, Response } from "express";
+import userRoutes from '../routes/user';
 import "reflect-metadata";
+import "../database";
+import 'express-async-errors';
+
 
 export default () => {
   const app = express();
+
   app.use(express.json());
-  app.use(testRouter);
+  app.use("/users", userRoutes);
+
+
 
   return app;
 }
