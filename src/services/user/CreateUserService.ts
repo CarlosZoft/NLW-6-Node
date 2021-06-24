62c10a5862bba913bf1b6c401324089273970d48
Carlos <carlosoftwar@gmail.com>
2021-06-24 00:48:39 -0300

    fix: admin default

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -12,7 +12,7 @@ interface IUserRequest {
 }
 
 export class CreateUserService {
-  async execute({ name, email, admin, password }: IUserRequest) {
+  async execute({ name, email, admin = false, password }: IUserRequest) {
     const userRepository = getCustomRepository(UserRepository);
 
     if (!email) {
